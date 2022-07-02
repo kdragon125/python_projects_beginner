0bb2b25952dc4918321e2956861e9461f185676b
kdragon125 <swordsora125@gmail.com>
2022-07-02 04:16:23 -0400

    gh

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,11 @@
 
+def translate(phrase):
+    translation = ''
+    for letter in phrase:
+        if letter in 'aeiouAEIOU':
+            translation = translation + 'g'
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input('enter a phrase: ')))
